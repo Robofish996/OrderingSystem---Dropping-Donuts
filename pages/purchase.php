c044123e3fcaa264317402d1631b20f65829da50
Matthew Riddle <Matthew.A.Riddle@outlook.com>
2023-06-16 17:51:56 +0200

    Updating last of purchase.css and purchase.php

--- pages/purchase.php
+++ pages/purchase.php
@@ -1,3 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/purchase.css">
+    <title>Thank you for your purchase</title>
+</head>
+<body>
+<div class="container">
 <?php
 // Check if the purchase order number is present
 if (isset($_POST['purchaseOrderNumber'])) {
@@ -15,7 +27,7 @@ if (isset($_POST['purchaseOrderNumber'])) {
         echo "<p>Redirecting to home page...</p>";
 
         // Redirect the user to the home page after a delay
-        header("refresh:10; url=home.php");
+       header("refresh:10; url=home.php");
         exit();
     } else {
         // Payment failed, display an error message
@@ -35,3 +47,6 @@ function simulatePayment()
     return true;
 }
 ?>
+</div>
+</body>
+</html>
